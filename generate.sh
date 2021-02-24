a93794cf72ad55bceb66a846c5a27a5d7a935c18
liyuan.ye <liyuan.ye@ecarx.com.cn>
2021-02-24 18:18:55 +0800

    利用更简单的方法来在clion中打开项目

--- generate.sh
+++ generate.sh
@@ -9,7 +9,7 @@ python $DIR/vscode-linux-kernel/generate_compdb.py  -O $1
 #fix the path of compile_commands.json
 sed  -i 's/\/work\//\/home\/ecarx\/work\//g' compile_commands.json
 sed  -i 's/\/home\/ecarx\/home\/ecarx\//\/home\/ecarx\//g' compile_commands.json
-$DIR/kernel-grok/generate_cmake -d $CURRENT_DIR
-echo -e "SET(CMAKE_C_COMPILER "gcc")\ninclude_directories(".")\ninclude_directories("./include")" >> CMakeLists.txt
+#$DIR/kernel-grok/generate_cmake -d $CURRENT_DIR
+#echo -e "SET(CMAKE_C_COMPILER "gcc")\ninclude_directories(".")\ninclude_directories("./include")" >> CMakeLists.txt
 
 
